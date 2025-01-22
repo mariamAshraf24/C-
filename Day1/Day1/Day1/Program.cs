fc6d07be39a6d9753b9d5e3ce130f910910a8d98
Mariam Ashraf <your.email@example.com>
2025-01-22 19:12:50 +0200

    day1

---
+++ Day1/Day1/Day1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static decimal Calculator(decimal FirstNumber, decimal SecondNumber, char Operator)
+        {
+            if (Operator == '+') {
+                      return FirstNumber + SecondNumber;
+            }
+            else if (Operator == '-'){
+                return FirstNumber - SecondNumber;
+            }
+            else if (Operator == '*'){
+                return FirstNumber * SecondNumber;
+            }
+            else if (Operator == '/')
+            {
+                return FirstNumber / SecondNumber;
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Operator");
+                return 0;
+            }
+        }
+        static void CompleteNumbers()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j <= i/2; j++)
+                {
+                    if(i%j==0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Enter FirstNumber");
+            decimal FirstNumber = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter SecondNumber");
+            decimal SecondNumber = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Opertar");
+            char Operator = char.Parse(Console.ReadLine());
+            Console.WriteLine(Calculator(FirstNumber, SecondNumber, Operator));
+
+            Console.WriteLine("Complete Numbers are :");
+            CompleteNumbers();
+        }
+
+    }
+}
